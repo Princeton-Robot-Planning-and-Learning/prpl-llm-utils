13706bd450c72fc030dec08f51e579c6afbb7094
Tom Silver <tomssilver@gmail.com>
2025-07-09 09:42:06 -0400

    wip

--- src/prpl_llm_utils/models.py
+++ src/prpl_llm_utils/models.py
@@ -1,23 +1,24 @@
 """Interfaces for large language models."""
 
 import abc
-import json
 import logging
 import os
-from pathlib import Path
 from typing import Hashable
 
 import openai
 import PIL.Image
 
+from prpl_llm_utils.cache import PretrainedLargeModelCache, ResponseNotFound
 from prpl_llm_utils.structs import Query, Response
 
 
 class PretrainedLargeModel(abc.ABC):
     """A pretrained large vision or language model."""
 
-    def __init__(self, cache_dir: Path, use_cache_only: bool = False) -> None:
-        self._cache_dir = cache_dir
+    def __init__(
+        self, cache: PretrainedLargeModelCache, use_cache_only: bool = False
+    ) -> None:
+        self._cache = cache
         self._use_cache_only = use_cache_only
 
     @abc.abstractmethod
@@ -51,67 +52,34 @@ class PretrainedLargeModel(abc.ABC):
         """
         # Create the query.
         query = Query(prompt, imgs=imgs, hyperparameters=hyperparameters)
-        # Set up the cache.
-        cache_dir = self._get_cache_dir(query)
-        if not (cache_dir / "prompt.txt").exists():
+        # Try to load from the cache.
+        model_id = self.get_id()
+        try:
+            response = self._cache.try_load_response(query, model_id)
+            logging.debug("Loaded model response from cache.")
+        except ResponseNotFound:
+            # No response found, so we need to query.
             if self._use_cache_only:
                 raise ValueError("No cached response found for prompt.")
             logging.debug(f"Querying model {self.get_id()} with new prompt.")
-            # Query the model.
             response = self._run_query(query)
-            # Cache the text prompt.
-            prompt_file = cache_dir / "prompt.txt"
-            with open(prompt_file, "w", encoding="utf-8") as f:
-                f.write(prompt)
-            # Cache the image prompt if it exists.
-            if imgs is not None:
-                imgs_folderpath = cache_dir / "imgs"
-                imgs_folderpath.mkdir(exist_ok=True)
-                for i, img in enumerate(imgs):
-                    filename_suffix = str(i) + ".jpg"
-                    img.save(imgs_folderpath / filename_suffix)
-            # Cache each response.
-            completion_file = cache_dir / "completion.txt"
-            with open(completion_file, "w", encoding="utf-8") as f:
-                f.write(response.text)
-            # Cache the metadata.
-            metadata_file = cache_dir / "metadata.json"
-            with open(metadata_file, "w", encoding="utf-8") as f:
-                json.dump(response.metadata, f)
-            logging.debug(f"Saved model response to {cache_dir}.")
-        # Load the saved completions.
-        completion_file = cache_dir / "completion.txt"
-        with open(completion_file, "r", encoding="utf-8") as f:
-            completion = f.read()
-        # Load the metadata.
-        metadata_file = cache_dir / "metadata.json"
-        with open(metadata_file, "r", encoding="utf-8") as f:
-            metadata = json.load(f)
-        # Create the response.
-        response = Response(completion, metadata)
-        logging.debug(f"Loaded model response from {cache_dir}.")
+            # Save the response to cache.
+            self._cache.save(query, model_id, response)
         return response
-
-    def _get_cache_dir(self, query: Query) -> Path:
-        # Set up the cache directory.
-        self._cache_dir.mkdir(exist_ok=True)
-        model_id = self.get_id()
-        query_id = query.get_id()
-        cache_foldername = f"{model_id}_{query_id}"
-        cache_folderpath = self._cache_dir / cache_foldername
-        cache_folderpath.mkdir(exist_ok=True)
-        return cache_folderpath
 
 
 class OpenAIModel(PretrainedLargeModel):
     """Common interface with methods for all OpenAI-based models."""
 
     def __init__(
-        self, model_name: str, cache_dir: Path, use_cache_only: bool = False
+        self,
+        model_name: str,
+        cache: PretrainedLargeModelCache,
+        use_cache_only: bool = False,
     ) -> None:
         self._model_name = model_name
         assert "OPENAI_API_KEY" in os.environ, "Need to set OPENAI_API_KEY"
-        super().__init__(cache_dir, use_cache_only)
+        super().__init__(cache, use_cache_only)
 
     def get_id(self) -> str:
         return self._model_name
@@ -146,11 +114,11 @@ class CannedResponseModel(PretrainedLargeModel):
     def __init__(
         self,
         query_to_response: dict[Query, Response],
-        cache_dir: Path,
+        cache: PretrainedLargeModelCache,
         use_cache_only: bool = False,
     ) -> None:
         self._query_to_response = query_to_response
-        super().__init__(cache_dir, use_cache_only)
+        super().__init__(cache, use_cache_only)
 
     def get_id(self) -> str:
         return "canned"
